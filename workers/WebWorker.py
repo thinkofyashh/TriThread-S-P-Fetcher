fe18d39eff7db8bd12a54e33ded4857d4984df70
yashrawat <yashrawat682@gmail.com>
2026-06-25 07:00:45 +0530

    initial Setup

---
+++ workers/WebWorker.py
@@ -0,0 +1,11 @@
+import threading 
+
+class WebWorker(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.url="" # Hardcoded URL for Wiki website 
+
+
+    def get_sp_500_companies():
+            pass
+    
