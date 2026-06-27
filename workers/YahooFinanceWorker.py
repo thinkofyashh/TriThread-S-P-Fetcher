1e85d624f2fe610cce722c6a83d1e9d14372e3c5
yashrawat <yashrawat682@gmail.com>
2026-06-27 16:17:28 +0530

    Moved towards the queue and Master Schedular logic and modified YahooFinanceWorker file

--- workers/YahooFinanceWorker.py
+++ workers/YahooFinanceWorker.py
@@ -2,19 +2,43 @@ import threading
 from bs4 import BeautifulSoup
 import requests 
 
-class YahooFinanceWorker(threading.Thread):
-    def __init__(self,symbol,**kwargs):
+
+class YahooFinanceWorkerScheduler(threading.Thread):
+    def __init__(self,input_queue):
+        super().__init__()
+        self.input_queue=input_queue
+
+
+    def run(self):
+        while True:
+            val=self.input_queue.get()
+            try:
+                if val=="DONE":
+                 break
+
+                yahooFinanceprice=YahooFinanceWorker(val)
+                price=yahooFinanceprice.getPrice()
+                print(price)
+            finally:
+                self.input_queue.task_done()
+                    
+
+
+
+
+class YahooFinanceWorker():
+    def __init__(self,symbol):
         super().__init__()
         self.symbol=symbol
         base_url="https://finance.yahoo.com/quote/"
         self.url=f'{base_url}{self.symbol}'
-        self.start()
+       
 
     def getPrice(self):
         headers = {
     "User-Agent": "Mozilla/5.0"
 }
-        response=requests.get(self.url,headers=headers)
+        response=requests.get(self.url,headers=headers,timeout=10)
         if response.status_code!=200:
             print(response.status_code)
             print("Unable to Fetech the Price of the Stock .")
@@ -27,8 +51,7 @@ class YahooFinanceWorker(threading.Thread):
             return price
         return "NaN"
 
-    def run(self):
-        p=self.getPrice()
-        print(p)    
+    
+          
 
 
